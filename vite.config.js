fd8a5af08ed990824848768220df288df6ad0d0e
wahyunoerr <wahyunoer1001@gmail.com>
2026-01-10 04:00:12 +0700

    refactor: Improve formatting and structure in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,31 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', '@inertiajs/vue3'],
+          sweetalert: ['sweetalert2'],
+          tinymce: ['@tinymce/tinymce-vue'],
+          utils: ['moment', 'xlsx', '@vuepic/vue-datepicker', '@chenfengyuan/vue-countdown'],
+        },
+      },
+    },
+  },
 });
